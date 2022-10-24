923a91e30abf923ec2aca5273d1a649d569c68a4
Dmitriy <dima.music1989@gmail.com>
2022-10-24 13:43:37 +0300

    first

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache")
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+})
 
 module.exports = nextConfig
